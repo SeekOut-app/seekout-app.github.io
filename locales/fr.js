a7abfe34846aa83712a0ebeeef2d36e140050553
SeekOut-app <134848273+SeekOut-app@users.noreply.github.com>
2023-06-07 23:42:02 +0200

    Update fr.js

--- locales/fr.js
+++ locales/fr.js
@@ -4,4 +4,5 @@
   "courseDescription": "Quelqu'un veut partager avec vous la course d'orientation qu'il a créé. Ouvrez l'application pour obtenir plus de détails.",
   "installTitle": "Installez l'application",
   "installDescription": "Créez et participez vous à des courses d'orientation avec SeekOut. Rejoignez l'aventure en installant l'application SeekOut gratuitement."
+  "openApp": "Ouvrir l'application"
 }
