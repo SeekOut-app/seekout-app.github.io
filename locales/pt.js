b71f93ace75fe08068aff6b4698c3c48c9d68479
SeekOut-app <134848273+SeekOut-app@users.noreply.github.com>
2023-06-08 12:53:48 +0200

    Create pt.js

---
+++ locales/pt.js
@@ -0,0 +1,8 @@
+{
+  "header": "Você recebeu uma corrida de orientação!",
+  "courseTitle": "Obter a corrida",
+  "courseDescription": "Alguém quer compartilhar com você a corrida de orientação que criou. Abra o aplicativo para obter mais detalhes.",
+  "installTitle": "Instale o aplicativo",
+  "installDescription": "Crie e participe de corridas de orientação com SeekOut. Junte-se à aventura instalando o aplicativo SeekOut gratuitamente.",
+  "openApp": "Abrir o aplicativo"
+}
