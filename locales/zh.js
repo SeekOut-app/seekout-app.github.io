3721ce73319ad9b51d4b25b496337ded68ad891f
SeekOut-app <134848273+SeekOut-app@users.noreply.github.com>
2023-06-08 12:58:24 +0200

    Create zh.js

---
+++ locales/zh.js
@@ -0,0 +1,8 @@
+{
+  "header": "您收到了一个定向赛！",
+  "courseTitle": "获取比赛",
+  "courseDescription": "有人想与您分享他创建的定向赛。打开应用程序以获取更多详细信息。",
+  "installTitle": "安装应用程序",
+  "installDescription": "使用SeekOut创建并参与定向赛。通过免费安装SeekOut应用程序，加入冒险之旅。",
+  "openApp": "打开应用程序"
+}
