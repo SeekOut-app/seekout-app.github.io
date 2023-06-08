6c6833839112e32c1315ccf6c03b35caf3072272
SeekOut-app <134848273+SeekOut-app@users.noreply.github.com>
2023-06-08 13:05:05 +0200

    Create pl.js

---
+++ locales/pl.js
@@ -0,0 +1,8 @@
+{
+  "header": "Otrzymałeś bieg orientacyjny!",
+  "courseTitle": "Pobierz bieg",
+  "courseDescription": "Ktoś chce się podzielić z Tobą biegiem orientacyjnym, który stworzył. Otwórz aplikację, aby uzyskać więcej szczegółów.",
+  "installTitle": "Zainstaluj aplikację",
+  "installDescription": "Twórz i bierz udział w biegach orientacyjnych z SeekOut. Dołącz do przygody, instalując bezpłatnie aplikację SeekOut.",
+  "openApp": "Otwórz aplikację"
+}
