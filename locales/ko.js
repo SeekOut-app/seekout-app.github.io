19d192934ac996967000e00d9063d4b15ef39abc
SeekOut-app <134848273+SeekOut-app@users.noreply.github.com>
2023-06-08 12:58:51 +0200

    Create ko.js

---
+++ locales/ko.js
@@ -0,0 +1,8 @@
+{
+  "header": "오리엔테이션 레이스를 받았습니다!",
+  "courseTitle": "레이스 받기",
+  "courseDescription": "누군가가 만든 오리엔테이션 레이스를 공유하고 싶어합니다. 자세한 내용을 확인하려면 앱을 열어주세요.",
+  "installTitle": "앱 설치",
+  "installDescription": "SeekOut을 사용하여 오리엔테이션 레이스를 생성하고 참여하세요. SeekOut 앱을 무료로 설치하여 모험에 참여해보세요.",
+  "openApp": "앱 열기"
+}
