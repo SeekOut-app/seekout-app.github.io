f26382831552a67c24034fbe7b30318bac4b8a61
SeekOut-app <134848273+SeekOut-app@users.noreply.github.com>
2023-06-08 12:55:27 +0200

    Create nl.js

---
+++ locales/nl.js
@@ -0,0 +1,8 @@
+{
+  "header": "Je hebt een oriëntatierun ontvangen!",
+  "courseTitle": "Haal de run op",
+  "courseDescription": "Iemand wil een oriëntatierun met je delen die hij heeft gemaakt. Open de app voor meer details.",
+  "installTitle": "Installeer de app",
+  "installDescription": "Maak en neem deel aan oriëntatieruns met SeekOut. Doe mee aan het avontuur door de SeekOut-app gratis te installeren.",
+  "openApp": "Open de app"
+}
